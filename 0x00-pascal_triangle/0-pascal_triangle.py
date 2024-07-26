dddc6f1b74963eb4465b9cb3db04ab37b24aa0d5
kerubo <48017280+aragakerubo@users.noreply.github.com>
2024-07-25 23:12:48 -0400

    0. Pascal's Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
+"""Module contains a function
+that returns the Pascals triangle"""
+
 
 def pascal_triangle(n):
-    if n <= 0:
-        return []
-    pascal = [[1]]
-    for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(pascal[i - 1][j - 1] + pascal[i - 1][j])
-        row.append(1)
-        pascal.append(row)
-    return pascal
+    """This function contains the function implementation"""
+    if (n <= 0):
+        return ([])
+    main_list = [[1]]
+    for i in range(0, (n - 1)):
+        sub_list = [1]
+        for j in range(0, i):
+            sub_list.append(main_list[i][j] + main_list[i][j + 1])
+        sub_list.append(1)
+        main_list.append(sub_list)
+    return (main_list)
 
+
+if __name__ == "__main__":
+    for row in pascal_triangle(5):
+        print("[{}]".format(",".join([str(x) for x in row])))
