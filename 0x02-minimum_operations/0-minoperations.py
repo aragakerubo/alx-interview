b703a1086a08d768c1fc40e90e4feca589a4d222
aragakerubo <aragakerubo@gmail.com>
2024-08-08 17:38:24 -0400

    0. Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -20,22 +20,24 @@ Example:
     Number of operations: 6
 
 Functions:
-- minOperations(n): Returns the minimum number of operations needed to reach `n` characters.
+- minOperations(n): Returns the minimum number of operations needed
+to reach `n` characters.
 """
 
+
 def minOperations(n):
-    """Calculate the minimum number of operations required to get n 'H' characters."""
+    """Calculate the minimum number of operations required
+    to get n 'H' characters."""
     if n <= 1:
         return 0
-    
+
     operations = 0
     divisor = 2
-    
+
     while n > 1:
         while n % divisor == 0:
             operations += divisor
             n //= divisor
         divisor += 1
-    
-    return operations
 
+    return operations
