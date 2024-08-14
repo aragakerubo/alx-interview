08370d8f65bc72ab1c404e8bc6d80433902480a1
aragakerubo <aragakerubo@gmail.com>
2024-08-13 23:05:06 -0400

    0. Log parsing

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Script that reads stdin line by line and computes metrics"""
+
+import sys
+
+
+def print_stats(file_size, status_codes):
+    print("File size: {}".format(file_size))
+    for key, value in sorted(status_codes.items()):
+        if value:
+            print("{}: {}".format(key, value))
+
+
+def main():
+    file_size = 0
+    status_codes = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0,
+    }
+    count = 0
+    try:
+        for line in sys.stdin:
+            count += 1
+            data = line.split()
+            try:
+                file_size += int(data[-1])
+            except:
+                pass
+            try:
+                if data[-2] in status_codes:
+                    status_codes[data[-2]] += 1
+            except:
+                pass
+            if count == 10:
+                print_stats(file_size, status_codes)
+                count = 0
+    except KeyboardInterrupt:
+        print_stats(file_size, status_codes)
+        raise
+
+
+if __name__ == "__main__":
+    main()
