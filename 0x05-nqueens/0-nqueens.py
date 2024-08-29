a3cc4fbe4eee7d0f0849f7cc22a716c159079167
aragakerubo <aragakerubo@gmail.com>
2024-08-29 18:40:37 -0400

    0. N queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,39 +1,62 @@
 #!/usr/bin/python3
 """N queens puzzle"""
+
+
 import sys
 
 
-def isSafe(board, row, col):
+def isSafe(board, row, col, n):
     """Check if a queen can be placed on board[row][col]"""
-    for q in range(col):
-        if board[q] is row or abs(board[q] - row) is abs(q - col):
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
             return False
     return True
 
 
-def solveNQ(board, col):
-    """Solve N queens problem"""
-    n = len(board)
-    if col is n:
-        print(str([[c, board[c]] for c in range(n)]))
-        return
+def solveNQUtil(board, col, n):
+    """Use backtracking to solve N queen problem"""
+    if col == n:
+        res = []
+        for i in range(n):
+            for j in range(n):
+                if board[i][j] == 1:
+                    res.append([i, j])
+        print(res)
+        return True
+    res = False
     for i in range(n):
-        if isSafe(board, i, col):
-            board[col] = i
-            solveNQ(board, col + 1)
+        if isSafe(board, i, col, n):
+            board[i][col] = 1
+            res = solveNQUtil(board, col + 1, n) or res
+            board[i][col] = 0
+    return res
+
+
+def solveNQ(n):
+    """Initialize the board and call solve
+    NQUtil for the solution"""
+    board = [[0 for j in range(n)] for i in range(n)]
+    if solveNQUtil(board, 0, n) is False:
+        return False
+    return True
 
 
 if __name__ == "__main__":
-    if len(sys.argv) is not 2:
+    if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
     try:
-        N = int(sys.argv[1])
+        n = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
-    if N < 4:
+    if n < 4:
         print("N must be at least 4")
         sys.exit(1)
-    board = [0 for i in range(N)]
-    solveNQ(board, 0)
+    solveNQ(n)
