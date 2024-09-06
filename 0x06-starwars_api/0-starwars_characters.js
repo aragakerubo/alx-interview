cedffb8ed919b892268e31e50920c103ad0a7961
aragakerubo <aragakerubo@gmail.com>
2024-09-05 22:17:47 -0700

    0. Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,21 +1,21 @@
 #!/usr/bin/node
-const request = require("request");
-const url = "https://swapi-api.hbtn.io/api/films/" + process.argv[2];
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
 request(url, async function (error, response, body) {
-    if (error) {
-        console.error("error:", error);
-    }
-    const film = JSON.parse(body);
-    for (const character of film.characters) {
-        const res = await new Promise((resolve, reject) => {
-            request(character, function (error, response, body) {
-                if (error) {
-                    reject(error);
-                }
-                resolve(JSON.parse(body));
-            });
-        });
-        console.log(res.name);
-    }
+  if (error) {
+    console.error('error:', error);
+  }
+  const film = JSON.parse(body);
+  for (const character of film.characters) {
+    const res = await new Promise((resolve, reject) => {
+      request(character, function (error, response, body) {
+        if (error) {
+          reject(error);
+        }
+        resolve(JSON.parse(body));
+      });
+    });
+    console.log(res.name);
+  }
 });
